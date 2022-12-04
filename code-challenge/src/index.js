2cccab7f1ea49bc6ce18ee49b059bb93d1590a84
MuchinaWamburia <lucymuchina20@gmail.com>
2022-12-04 21:46:28 +0300

    added patch

--- code-challenge/src/index.js
+++ code-challenge/src/index.js
@@ -1,23 +1,25 @@
-// Your code here
+// // Your code here
 document.addEventListener("DOMContentLoaded", () => {
   fetchData();
 });
+// updating url to enable user view characters
+const base_url = "http://localhost:3000/characters";
 const characterBar = document.getElementById("character-bar");
 const characterName = document.getElementById("name");
 const characterImage = document.getElementById("image");
 const characterVoteCount = document.getElementById("vote-count");
 const characterVoteForm = document.getElementById("votes-form");
 
-//Get characters
+//accessing characters
 function fetchData() {
-  fetch("http://localhost:3000/characters")
+  fetch(base_url)
     .then((response) => response.json())
     .then((data) => {
       renderCharacters(data);
-    });
+        });
 }
 
-//upload  the characters  to html
+//Displaying characters on the page 
 function renderCharacters(data) {
   data.forEach((data) => {
     const nameSpan = document.createElement("span");
@@ -31,5 +33,44 @@ function renderCharacters(data) {
     });
   });
 }
+//votes
+characterVoteForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const newVotes = parseInt(event.target.votes.value);
+  const characterVoteCount = document.getElementById("vote-count");
+  let current = parseInt(characterVoteCount.textContent);
+  let votecount = (current += newVotes);
+  characterVoteCount.innerText = votecount;
+    
+  let updateVotes = {
+    votes: votecount,
+  };
+
+ });
+
+const resetButton = document.querySelector('#reset-btn')
+resetButton.addEventListener('click', (event) => {
 
 
+ // patch:allows partial updates for HTTP APIs in a standards compliant way.
+ //application/json: Indicates that the request body format is JSON
+ //stringify:converts a JavaScript value to a JSON string,
+  fetch(base_url)
+  .then(response => response.json())
+  .then(characters => {
+      //const charName = document.querySelector('#name')
+      const charID = characters.find(character => character.name === characterName.textContent)
+      fetch(`${base_url}/${charID.id}`, {
+          method: 'PATCH',
+          headers: {
+              'Content-Type' : 'application/json'
+          },
+          body: JSON.stringify({
+              votes : '0'
+          })
+      })
+      .then(response => response.json())
+      .then(data => characterVotes.textContent = data.votes)
+      .then((json) => console.log(json));
+  })
+})
